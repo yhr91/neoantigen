c061c51a3402c9b76fd8b08e389af6e21843aa18
Yusuf Roohani <yusuf.roohani@gmail.com>
2020-09-02 17:43:05 -0700

    saving all outputs to a single file

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -34,11 +34,31 @@ def correct_file(f, new_path):
         if text[i][0:7]=='Protein':
             text[i] = correction(text[i])
         i+=1
+    
+    return text[46:]
 
-    new_name = new_path + '/'+ f.split('/')[-1]
-    with open(new_name,"w") as file: 
-        file.writelines(text)
-        
-for i,f in enumerate(files):
-    print(i)
-    correct_file(f, new_path)
+def add_file_header(text, filename_dict):
+    with open(first, "r+") as read:
+        complete_text=read.readlines()[:46]
+    complete_text.extend(text)
+    return complete_text
+
+# For sorting filenames
+filename_dict = {int(f.split('_')[-1]):f 
+                 for f in files if f.split('/')[-1][0]=='H'}
+first = filename_dict[0]
+
+# Read and correct each file
+full_text = []
+for idx in sorted(filename_dict):
+    print(idx)
+    full_text.extend(correct_file(filename_dict[idx], new_path))
+    
+# Add file header
+full_text = add_file_header(full_text, first)
+
+# Write complete file
+new_name = (new_path + '/'+ 
+            first.split('/')[-1].split('_out_')[0])
+with open(new_name,"w") as file: 
+    file.writelines(full_text)
